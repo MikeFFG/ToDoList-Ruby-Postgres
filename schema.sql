d73bad30c2e5ae10ac793bbadb2e34621d84478d
Michael Burke <mike.d.burke@gmail.com>
2016-07-10 19:23:07 -0400

    Add database persistence class

--- schema.sql
+++ schema.sql
@@ -1,11 +1,11 @@
 CREATE TABLE lists (
   id serial PRIMARY KEY,
-  memo text NOT NULL UNIQUE
+  name text NOT NULL UNIQUE
 );
 
 CREATE TABLE todos (
   id serial PRIMARY KEY,
-  memo text NOT NULL,
+  name text NOT NULL,
   completed boolean NOT NULL DEFAULT false,
   list_id integer NOT NULL REFERENCES lists (id)
 );
